025aae765848a01d65b202a5d3c02bc94ebab9ca
Sup3r-Us3r <maydersonmello@gmail.com>
2022-01-16 12:54:22 -0300

    create environment for api with docker and docker compose

---
+++ api/.docker/mongo/init.js
@@ -0,0 +1,38 @@
+db.routes.insertMany([
+  {
+    _id: '1',
+    title: 'Primeiro',
+    startPosition: {
+      latitude: -15.82594,
+      longitude: -47.92923,
+    },
+    endPosition: {
+      latitude: -15.82942,
+      longitude: -47.92765,
+    },
+  },
+  {
+    _id: '2',
+    title: 'Segundo',
+    startPosition: {
+      latitude: -15.82449,
+      longitude: -47.92756,
+    },
+    endPosition: {
+      latitude: -15.8276,
+      longitude: -47.92621,
+    },
+  },
+  {
+    _id: '3',
+    title: 'Terceiro',
+    startPosition: {
+      latitude: -15.82331,
+      longitude: -47.92588,
+    },
+    endPosition: {
+      latitude: -15.82758,
+      longitude: -47.92532,
+    },
+  },
+]);
